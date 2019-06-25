ffe45584f7c0cb26615ae538bb1675b8f67c8270
Parul Saini <37900145+parulsaini42@users.noreply.github.com>
2019-06-25 11:23:47 +0100

    Update parse1.py

--- neuralnw/parse1.py
+++ neuralnw/parse1.py
@@ -17,10 +17,26 @@ def parameter_parser():
                         nargs = "?",
                         default = "./data/grid_vector.txt",
                         help = "vector path.")
-
-
-    
-
+    parser.add_argument("--epoch",
+                        type = int,
+                        default = 10,
+                        help = "number of epochs")
+    parser.add_argument("--batch",
+                        type = int,
+                        default = 100,
+                        help = "batch size")
+    parser.add_argument("--hidden",
+                        type = int,
+                        default = 100,
+                        help = "hidden layer size")
+    parser.add_argument("--threshold",
+                        type = float,
+                        default = 0.5,
+                        help = "threshold for finding prediction values")
+    parser.add_argument("--lr",
+                        type = float,
+                        default = 0.001,
+                        help = "learning rate")
 
     return parser.parse_args()
 
